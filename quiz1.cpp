cfe8ed41059967030a89236f831e4c43bc292363
anna-cpu <67580871+anna-cpu@users.noreply.github.com>
2021-01-15 22:58:46 +0800

    Add files via upload

---
+++ quiz1.cpp
@@ -0,0 +1,23 @@
+// good loking
+cout <<"\t\t==========*******************************************=========="<<endl;
+            cout<<"\t\t\t\t  Blood Donation Managment System   "<<endl;
+cout <<"\t\t==========*******************************************=========="<<endl;
+             cout << "\t\t\t\t Enter 1 to create new Donor:"<<endl;	
+             cout << "\t\t\t\t Enter 2 to update  Donor details:"<<endl;	
+             cout << "\t\t\t\t Enter 3 to delete Donor:"<<endl;
+             cout << "\t\t\t\t Enter in 5 to exit:"<<endl;
+cout <<"\t\t============================================================="<<endl;
+cout <<"\t\t============================================================="<<endl;	
+
+
+
+
+		  	cout <<" are certain you want to delete donor "	<<array[temp].name << " with ID number " << array[temp].id << " ? "<<endl;
+		  	cout << "Enter 1 for yes and any key for no " <<endl;
+		  	cin>> sure;
+		  	  if (sure ==1)
+		  	  {cout << " donor succefully deleted" <<endl;
+			  }
+			  else 
+			  cout << " donor not deleted" <<endl;
+
